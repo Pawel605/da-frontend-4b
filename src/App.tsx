914ed9f775e2774d43cc691a77131f2e26070ede
Paweł F <pawel605@gmail.com>
2022-05-23 19:58:49 +0200

    Add new form (input) component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import {InputForm} from "./InputForm";
 
 function App() {
   return (
       <div className="App">
         <header className="App-header">
+            <InputForm defaultValue={"default form"}/>
         </header>
       </div>
   );
